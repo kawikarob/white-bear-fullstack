7e8db850b8de55c95aeea89ac309bb21e81fbbf4
kawikarob <64801223+kawikarob@users.noreply.github.com>
2020-07-20 15:20:22 -0700

    port change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("*", (req, res) => {
    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3030;
 app.listen(port, () =>
    console.log(`Server running at http://localhost:${port}`)
 );
